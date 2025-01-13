46f485192a262afa6ee6c0a71f557de0b00cc6d8
Jeffrey <jsyme16@gmail.com>
2025-01-13 13:18:59 -0500

    fixed a bunch of issues with imports

--- src/routes/use-page.ts
+++ src/routes/use-page.ts
@@ -1,6 +1,15 @@
-import { useEffect } from 'react'
+import { useCallback, useEffect } from 'react'
+import { queryProductCategories } from '../lib'
 
 export default function usePage() {
+
+    const getProductCategories = useCallback(async () => {
+        const productCategories = await queryProductCategories()
+
+        console.log(productCategories)
+    }, [])
+
+    useEffect(() => { getProductCategories() }, [])
     useEffect(() => {
         document.title = 'Adventure Works - Home'
     }, [])
