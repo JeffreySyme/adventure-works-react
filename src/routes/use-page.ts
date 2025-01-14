23eed351a039cabc6e8cd2b57a2b32abcffe53a5
Jeffrey <jsyme16@gmail.com>
2025-01-14 11:22:24 -0500

    Setting breadcrumbs on mount for the home page

--- src/routes/use-page.ts
+++ src/routes/use-page.ts
@@ -1,8 +1,22 @@
-import { useEffect } from 'react'
+import { useCallback, useEffect } from 'react'
+import { useAppDispatch } from '../store'
+import { setBreadcrumbs } from './store'
 
 export default function usePage() {
+    const dispatch = useAppDispatch()
 
+    const updateBreadcrumbs = useCallback(() => {
+        dispatch(setBreadcrumbs([
+            {
+                active: true,
+                text: 'Home',
+                to: '/',
+            }
+        ]))
+    }, [dispatch])
+    
+    useEffect(() => { updateBreadcrumbs() }, [updateBreadcrumbs])
     useEffect(() => {
-        document.title = 'Adventure Works - Home'
+        document.title = 'Adventure Works - Home' 
     }, [])
 }
